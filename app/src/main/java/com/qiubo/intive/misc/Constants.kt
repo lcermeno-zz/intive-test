cbd54ad771bd0b44e184478118a08484bb911b75
Lawrence Cermeno <lcermeno.salcedo@gmail.com>
2019-09-18 12:34:25 -0300

    updated page size

--- app/src/main/java/com/qiubo/intive/misc/Constants.kt
+++ app/src/main/java/com/qiubo/intive/misc/Constants.kt
@@ -1,8 +1,9 @@
 package com.qiubo.intive.misc
 
 object Constants {
+    const val INDEX_NOT_FOUND = -1
     const val ITEM_KEY = "item"
     const val LIMIT_KEY = "results"
     const val PAGE_KEY = "page"
-    const val PAGE_LIMIT = 20
+    const val PAGE_LIMIT = 40
 }
