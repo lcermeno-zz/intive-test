3823481c153303e627d7f2b31176f9ff3740ea4b
Lawrence Cermeno <lcermeno.salcedo@gmail.com>
2019-09-17 21:30:11 -0300

    added class to save keys and constants values

---
+++ app/src/main/java/com/qiubo/intive/misc/Constants.kt
@@ -0,0 +1,8 @@
+package com.qiubo.intive.misc
+
+object Constants {
+    const val ITEM_KEY = "item"
+    const val LIMIT_KEY = "results"
+    const val PAGE_KEY = "page"
+    const val PAGE_LIMIT = 20
+}
