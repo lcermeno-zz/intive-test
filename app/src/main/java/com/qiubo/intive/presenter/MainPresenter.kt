a7f1bcf607b26e4703d04118cdc8f9e23ac7a242
Lawrence Cermeno <lcermeno.salcedo@gmail.com>
2019-09-18 08:50:30 -0300

    added logic to load the images and set dynamically the item size in the grid

--- app/src/main/java/com/qiubo/intive/presenter/MainPresenter.kt
+++ app/src/main/java/com/qiubo/intive/presenter/MainPresenter.kt
@@ -8,7 +8,11 @@ import com.qiubo.intive.view.IMainView
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
-class MainPresenter(private var mView: IMainView, private val mGetUserUseCase: GetUserUseCase) :
+class MainPresenter(
+    private var mView: IMainView?,
+    private val mGetUserUseCase: GetUserUseCase,
+    private val width: Int
+) :
     IMainPresenter {
     private var mPage = 1
     private var mLoading = false
@@ -16,7 +20,6 @@ class MainPresenter(private var mView: IMainView, private val mGetUserUseCase: G
     companion object {
         private const val TAG = "MainPresenter"
     }
-
 
     override fun getUsers() {
         if (!mLoading) {
@@ -36,18 +39,55 @@ class MainPresenter(private var mView: IMainView, private val mGetUserUseCase: G
 
     @SuppressLint("CheckResult")
     private fun requestData(list: MutableList<User>, function: (List<User>) -> Unit) {
-        mGetUserUseCase
-            .getAll(mPage)
+        mLoading = true
+        mGetUserUseCase.getAll(mPage)
+            .map {
+
+                var row = 0
+
+                for ((index, User) in it.results.withIndex()) {
+
+                    if (index % 3 == 0) row++
+
+                    when (row % 3) {
+                        0 -> assignItemSize(1, index, User)
+                        1 -> assignItemSize(2, index, User)
+                        2 -> assignItemSize(3, index, User)
+                    }
+                }
+                it
+            }
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({
-                Log.d(TAG, "Received User list with size: ${it.results.size}")
+                Log.d(TAG, "Received album list with size: ${it.results.size}")
                 list.addAll(it.results)
                 function(list)
+                mLoading = false
             }, {
                 Log.e(TAG, it.message)
                 it.printStackTrace()
                 it.message?.let { message -> mView?.onError(message) }
             })
+    }
+
+    private fun assignItemSize(option: Int, index: Int, User: User) {
+        when (option) {
+            1 -> when {
+                index % 3 == 0 -> User.size = width * 0.5f
+                index % 3 == 1 -> User.size = width * 0.3f
+                else -> User.size = width * 0.2f
+            }
+            2 -> when {
+                index % 3 == 0 -> User.size = width * 0.2f
+                index % 3 == 1 -> User.size = width * 0.5f
+                else -> User.size = width * 0.3f
+            }
+            3 -> when {
+                index % 3 == 0 -> User.size = width * 0.3f
+                index % 3 == 1 -> User.size = width * 0.2f
+                else -> User.size = width * 0.5f
+            }
+        }
     }
 }
