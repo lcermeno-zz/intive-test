05df17f2a7a0aabd1f28c4c95bf97ddad9343f0f
Lawrence Cermeno <lcermeno.salcedo@gmail.com>
2019-09-18 08:44:36 -0300

    added ScreenHelper to get dimensions of the screen

---
+++ app/src/main/java/com/qiubo/intive/misc/ScreenHelper.kt
@@ -0,0 +1,17 @@
+package com.qiubo.intive.misc
+
+import android.app.Activity
+import android.util.DisplayMetrics
+
+object ScreenHelper {
+
+    fun getScreenRealWidth(activity: Activity): Int {
+        val displayMetrics = DisplayMetrics()
+        setupMetrics(activity, displayMetrics)
+        return displayMetrics.widthPixels
+    }
+
+    private fun setupMetrics(activity: Activity, displayMetrics: DisplayMetrics) {
+        activity.windowManager.defaultDisplay.getRealMetrics(displayMetrics)
+    }
+}
