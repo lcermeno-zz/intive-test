611586b6c2a13befcb8b173951ce4d2f9f563e5d
Lawrence Cermeno <lcermeno.salcedo@gmail.com>
2019-09-17 22:57:54 -0300

    added new fields to user entity

--- app/src/main/java/com/qiubo/intive/entities/RandomUser.kt
+++ app/src/main/java/com/qiubo/intive/entities/RandomUser.kt
@@ -2,7 +2,14 @@ package com.qiubo.intive.entities
 
 data class RandomUserResponse(val results: List<User>)
 data class User(
+    val id: Id,
+    val name: Name,
+    val picture: Picture,
     val gender: String,
     val email: String,
     val nat: String
 )
+
+data class Name(val title: String, val first: String, val last: String)
+data class Id(val name: String, val value: String)
+data class Picture(val large: String, val medium: String, val thumbnail: String)
