fb67959f0db5375dc34045b93fbe6d3672897ce4
Lawrence Cermeno <lcermeno.salcedo@gmail.com>
2019-09-17 21:16:21 -0300

    added HttpClient factory using retrofit

---
+++ app/src/main/java/com/qiubo/intive/api/HttpClientFactory.kt
@@ -0,0 +1,20 @@
+package com.qiubo.intive.api
+
+import com.qiubo.intive.BuildConfig
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+object HttpClientFactory {
+
+    private val mRetrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BuildConfig.API_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+    }
+
+    fun <T> getService(serviceClass: Class<T>): T = mRetrofit.create(serviceClass)
+}
